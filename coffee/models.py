28fd96d41f3df4108212d1bbbd5fcb030b6271c4
Bekhzodcoder <bexzodcoder@gmail.com>
2022-12-13 22:15:16 +0500

    django

---
+++ coffee/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Coffee(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    quantity = models.IntegerField()
+    image = models.CharField(max_length=2083)
